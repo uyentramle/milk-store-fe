46fa0af216fdbb57e2153aee8a55b5e712f400d1
hoaq139 <hoangdn1309@gmail.com>
2024-06-22 22:25:13 +0700

    add cart page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,9 @@ import SignUpPage from '../pages/client/SignUp/SignUpPage';
 import SignInPage from '../pages/client/SignIn/SignInPage';
 import ForgotPasswordPage from '../pages/client/ForgotPassword/ForgotPasswordPage';
 import { DefaultLayoutProps } from '../types/layout.type';
+import CartPage from '../pages/client/Cart/Cart';
+
+
 
 interface RouteProps {
     path: string;
@@ -25,6 +28,7 @@ const publicRoutes: RouteProps[] = [
     { path: '/blog', component: BlogPage, layout: DefaultClientLayout },
     { path: '/blogdetail', component: BlogPageDetail, layout: DefaultClientLayout },
     { path: '/productdetail', component: ProductDetail, layout: DefaultClientLayout },
+    { path: '/cart', component: CartPage, layout: DefaultClientLayout },
 ];
 
 const privateRoutes: RouteProps[] = [];
