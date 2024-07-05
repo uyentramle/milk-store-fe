45f4ec7aa093144a0d4d23b1205c1202de9fa6d7
huahao333 <huahao04@gmail.com>
2024-07-05 21:43:40 +0700

    Blog detail done

--- src/pages/client/Blog/BlogPage.tsx
+++ src/pages/client/Blog/BlogPage.tsx
@@ -7,7 +7,8 @@ import { LogoutOutlined, SearchOutlined, ShoppingCartOutlined } from '@ant-desig
 import { Button } from 'antd';
 import { FaHeart, FaShareAlt, FaComment, FaFontAwesome } from 'react-icons/fa';
 
-const { Title, Paragraph } = Typography;
+
+
 
 const BlogPage = () => {
     const Blog = [
@@ -16,25 +17,25 @@ const BlogPage = () => {
             image: "https://www.shutterstock.com/image-photo/happy-curly-millennial-indian-man-600nw-2140413095.jpg",
             name: "Blog  1 ",
             title: "What you know about",
-            author:"Mr.Milk",
-            Date:"11/4"
+            author: "Mr.Milk",
+            Date: "11/4"
 
         },
         {
             id: 2,
-    image: "https://www.shutterstock.com/image-photo/happy-curly-millennial-indian-man-600nw-2140413095.jpg",
-    name: "Introduction to the Basics",
-    title: "Understanding the Essentials",
-    author: "Author X",
-    Date: "April 11th"
+            image: "https://www.shutterstock.com/image-photo/happy-curly-millennial-indian-man-600nw-2140413095.jpg",
+            name: "Introduction to the Basics",
+            title: "Understanding the Essentials",
+            author: "Author X",
+            Date: "April 11th"
         },
         {
             id: 3,
-    image: "https://www.shutterstock.com/image-photo/happy-curly-millennial-indian-man-600nw-2140413095.jpg",
-    name: "Advanced Techniques in",
-    title: "Exploring the Depths of",
-    author: "Ms. Expert",
-    Date: "2023-06-18"
+            image: "https://www.shutterstock.com/image-photo/happy-curly-millennial-indian-man-600nw-2140413095.jpg",
+            name: "Advanced Techniques in",
+            title: "Exploring the Depths of",
+            author: "Ms. Expert",
+            Date: "2023-06-18"
         },
         {
             id: 4,
@@ -49,81 +50,81 @@ const BlogPage = () => {
             image: "https://cdn1.concung.com/2022/03/44602-84158-large_mobile/vinamilk-yoko-gold-2-1-2-tuoi-850g.png",
             name: "Blog  1 ",
             title: "What you know about",
-            author:"Mr.Milk",
-            Date:"11/4"
+            author: "Mr.Milk",
+            Date: "11/4"
         },
         {
             id: 6,
             image: "https://cdn1.concung.com/2022/05/57282-88158-large_mobile/vinamilk-optimum-gold-2-800g.png",
             name: "Blog  1 ",
             title: "What you know about",
-            author:"Mr.Milk",
-            Date:"11/4"
+            author: "Mr.Milk",
+            Date: "11/4"
         },
         {
             id: 7,
             image: "https://cdn1.concung.com/2022/05/57286-88164-large_mobile/vinamilk-optimum-gold-3-850g-1-2-tuoi.png",
             name: "Blog  1 ",
             title: "What you know about",
-            author:"Mr.Milk",
-            Date:"11/4"
+            author: "Mr.Milk",
+            Date: "11/4"
         },
         {
             id: 8,
             image: "https://cdn1.concung.com/2024/04/44603-1714120962-trans.png",
             name: "Blog  1 ",
             title: "What you know about",
-            author:"Mr.Milk",
-            Date:"11/4"
+            author: "Mr.Milk",
+            Date: "11/4"
         },
     ];
     return (
         <div className="min-h-screen flex flex-col">
-        <Banner />
-        <div className="flex flex-1">
-            <Sidebar />
-            <section className="container mx-auto m-4" style={{marginTop: '10px'}}>
-            <Row gutter={[16, 16]} >
-        {Blog.map((blog) => (
-            <Col key={blog.id} xs={24} sm={12} md={8} lg={6}>
-                <Link to="/blogdetail">
-                    <Card
-                        hoverable
-                        cover={<img alt={blog.name} src={blog.image} className='p-4' style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '23px' }} />}
-                    >
-                        
-                         <div style={{ display: 'flex'}}>
-                                        <FaHeart style={{ marginRight: '10px' }} />
-                                        <FaShareAlt style={{ marginRight: '10px', justifyContent:'center'}} />
-                                        <FaComment />
-                                    </div>
-                        <Card.Meta
-                            title={<h1  className="hover:text-green-800">{blog.name}</h1>}
-                            description={
-                                <div>
-                                    <div className="flex items-center"  style={{ display: 'flex', alignItems: 'center' }}>
-                                        <img
-                                            src={blog.image} // Assuming you want to use the same image, change this as needed
-                                            alt="Author"
-                                            style={{ width: '30px', height: '30px', borderRadius: '50%' }}
-                                        />
-                                        <div className="font-medium dark:text-black">
-                                        <span style={{ marginLeft: '10px', display: 'block' }} className="text-sm text-gray-500 dark:text-blacks-400">{blog.author}</span>
-                                        <span style={{ marginLeft: '10px', display: 'block' }} className="text-sm text-gray-500 dark:text-gray-400">{blog.Date}</span>
+            <Banner />
+            <div className="flex flex-1">
+                <Sidebar />
+                <section className="container mx-auto m-4" style={{ marginTop: '10px' }}>
+                    <Row gutter={[16, 16]} >
+                        {Blog.map((blog) => (
+                            <Col key={blog.id} xs={24} sm={12} md={8} lg={6}>
+                                <Link to="/blogdetail">
+                                    <Card
+                                        hoverable
+                                        cover={<img alt={blog.name} src={blog.image} className='p-4' style={{ width: '120%', height: '200px', objectFit: 'cover', borderRadius: '23px' }} />}
+                                    >
+
+                                        <div style={{ display: 'flex' }}>
+                                            <FaHeart style={{ marginRight: '10px' }} />
+                                            <FaShareAlt style={{ marginRight: '10px', justifyContent: 'center' }} />
+                                            <FaComment />
                                         </div>
-                                    </div>
-                                    
-                                </div>
-                            }
-                        />
-                    </Card>
-                </Link>
-            </Col>
-        ))}
-        </Row>
-    </section>
-</div>
-</div>
+                                        <Card.Meta
+                                            title={<h1 className="hover:text-green-800">{blog.name}</h1>}
+                                            description={
+                                                <div>
+                                                    <div className="flex items-center" style={{ display: 'flex', alignItems: 'center' }}>
+                                                        <img
+                                                            src={blog.image} // Assuming you want to use the same image, change this as needed
+                                                            alt="Author"
+                                                            style={{ width: '30px', height: '30px', borderRadius: '50%' }}
+                                                        />
+                                                        <div className="font-medium dark:text-black">
+                                                            <span style={{ marginLeft: '10px', display: 'block', fontSize:'13px'}} className="text-sm text-gray-500 dark:text-blacks-400">{blog.author}</span>
+                                                            <span style={{ marginLeft: '10px', display: 'block', fontSize:'10px' }} className="text-sm text-gray-500 dark:text-gray-400">{blog.Date}</span>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            }
+                                        />
+                                    </Card>
+                                </Link>
+                            </Col>
+                        ))}
+                    </Row>
+                </section>
+            </div>
+        </div>
 
     );
 };
--- src/pages/client/Blog/BlogPageDetail.tsx
+++ src/pages/client/Blog/BlogPageDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../../layouts/client/Components/Banner/Banner';
 import Sidebar from '../../../layouts/client/Components/Sidebar/Sidebar';
 import { Typography, Row, Col, Card } from 'antd';
+import { FaComment, FaHeart, FaShareAlt } from 'react-icons/fa';
 const { Title, Paragraph } = Typography;
 
 const BlogPageDetail = () => {
@@ -14,11 +15,66 @@ const BlogPageDetail = () => {
         </Col>
         <Col span={18}>
           <Card>
-            <Title level={2}>Blog Title Here</Title>
-            <Paragraph>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Title level={2}>Những điều cần biết về sữa</Title>
+              <Title className="text-sky-400/0" style={{ fontSize: '10px' }} level={5}>Đăng ngày: 10/9</Title>
+
+            </div>
+
+            <div style={{ marginTop: '20px', marginBottom: '20px' }} className="basis-5/7 ml-4">
+              <div className="flex items-left gap-4">
+                <img className="w-10 h-10 rounded-full"
+                  src="https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg" alt="" />
+                <div className="font-medium dark:text-black">
+                  <div>Mèo cam</div>
+
+                </div>
+              </div>
+            </div >
+            <Card>
+              <img
+                alt="example"
+                src="https://media.post.rvohealth.io/wp-content/uploads/2023/09/3154668-Clone-Market-Sep-T4-8-Of-The-Best-Baby-Formulas-1296x728-Header-e0edef.jpg"
+                style={{ width: '100%', height: 'auto' }}
+              />
+
+            </Card>
+            <Paragraph style={{ marginTop: '10px' }}>
               This is the detail view of your blog post. Here you can display the full content of your blog post.
               Add images, text, and other elements as needed to fully express your ideas.
             </Paragraph>
+            <div style={{ display: 'flex', marginTop: '20px' }}>
+              <div style={{ marginRight: '20px', display: 'flex', alignItems: 'center' }}>
+              <FaHeart />
+              <span style={{ marginLeft: '5px' }}>123</span> {/* Likes count */}
+            </div>
+            <div style={{ marginRight: '20px', display: 'flex', alignItems: 'center' }}>
+              <FaShareAlt />
+              <span style={{ marginLeft: '5px' }}>456</span> {/* Shares count */}
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <FaComment />
+              <span style={{ marginLeft: '5px' }}>789</span> {/* Comments count */}
+            </div>
+            </div>
+            <Title style={{marginTop: '20px'}} level={4}>Bình luận</Title>
+            <Card style={{ marginTop: '10px' }}>
+              <div className="comment">
+                <div className="avatar"></div>
+                <div className="comment-text">
+                  <p className="username">Tên người dùng</p>
+                  <p>Nội dung bình luận</p>
+                </div>
+              </div>
+
+              <div className="comment">
+                <div className="avatar"></div>
+                <div className="comment-text">
+                  <p className="username">Tên người dùng</p>
+                  <p>Nội dung bình luận</p>
+                </div>
+              </div>
+            </Card>
             {/* Add more content here as needed */}
           </Card>
         </Col>
